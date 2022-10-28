854622f77e4328f006a0b752c088fb9e1ee56db0
shine-lzx <1597395958@qq.com>
2022-10-28 14:13:03 +0800

    update

--- miniprogram/pages/home/index.ts
+++ miniprogram/pages/home/index.ts
@@ -4,10 +4,4 @@ Page<DataOptions, WechatMiniprogram.Page.CustomOption>({
   data: {
     scrollTop: 0,
   },
-
-  onPageScroll({ scrollTop }) {
-    this.setData({
-      scrollTop,
-    })
-  },
 })
